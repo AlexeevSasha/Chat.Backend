0a2ead5326009ef714a65efd21c350a397f4272e
Alexeev Alexander <work.alexeevs@gmail.com>
2023-02-16 15:54:46 +0200

    get user

--- src/auth/strategies/access.token.strategy.ts
+++ src/auth/strategies/access.token.strategy.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { IJwtPayload } from '../interfaces/jwt.payload';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../../user/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.ACCESS_TOKEN_SECRET,
     });
   }
 
-  validate(payload: IJwtPayload) {
-    return payload;
+  async validate(payload: IJwtPayload) {
+    return await this.userRepository.findOneBy({ id: payload.id });
   }
 }
